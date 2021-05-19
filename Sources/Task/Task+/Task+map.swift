f7924ee6981a9b076eace5d482d61c5fef90d818
Zach Eriksen <zmeriksen@icloud.com>
2021-05-18 20:28:31 -0500

    flatMap Task.do

--- Sources/Task/Task+/Task+map.swift
+++ Sources/Task/Task+/Task+map.swift
@@ -3,22 +3,26 @@ import Combine
 public extension Publisher {
     func `try`(
         action: @escaping (Output) throws -> Void
-    ) -> AnyPublisher<Output, Error> {
-        tryMap { data in
-            try action(data)
-            
-            return data
+    ) -> AnyPublisher<Output, Failure> where Failure == Error {
+        flatMap { data in
+            Task.do {
+                try action(data)
+                
+                return data
+            }
         }
         .eraseToAnyPublisher()
     }
     
     func `do`(
         action: @escaping (Output) -> Void
-    ) -> AnyPublisher<Output, Failure> {
-        map { data in
-            action(data)
-            
-            return data
+    ) -> AnyPublisher<Output, Failure> where Failure == Error {
+        flatMap { data in
+            Task.do {
+                action(data)
+                
+                return data
+            }
         }
         .eraseToAnyPublisher()
     }
